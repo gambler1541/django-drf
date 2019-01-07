4c3a6ef2c351710c0083518395b4b471b6ea18e2
pmb <gambler1541@gmail.com>
2019-01-08 00:10:53 +0900

    Snippet_list api 구현

--- app/snippets/views.py
+++ app/snippets/views.py
@@ -1,11 +1,31 @@
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 
 from .models import Snippets
 from .serializers import SnippetSerializer
 
 
+# CSRF검증에서 제외되는 view
+@csrf_exempt
 def snippet_list(request):
     if request.method == 'GET':
         snippet = Snippets.objects.all()
+        # Snippets QuerySet을 생성자로 사용한 SnippetSerializer 인스턴스
         serializer = SnippetSerializer(snippet, many=True)
+        # JSON형식의 문자열을 HttpResponse로 돌려줌 (content_type에 'application/json'명시됨)
         return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        # request를 분석해서 전달받은 JSON형식 문자열을 파이썬 데이터형으로 파싱
+        data = JSONParser().parse(request)
+        # data인수를 채우면서 Serializer인스턴스 생성 (역직렬화 과정)
+        serializer = SnippetSerializer(data=data)
+        # Seiralizer의 validation
+        if serializer.is_valid():
+            # valid한 경우, Serializer의 save()메서드로 새 Snippet인스턴스 생
+            serializer.save()
+            # 생성 후 serializer.data로 직렬화한 데이터를 JSON방식으로 리턴하며 201(created)상태코드 전달
+            return JsonResponse(serializer.data, status=201)
+        # invalid한 경우, error목록을 JSON형식으로 리턴하며 400(Bad request)
+        return JsonResponse(serializer.errors, status=400)
