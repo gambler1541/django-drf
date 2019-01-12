e58fa3aee84f3110be91109007830983e7de9683
pmb <gambler1541@gmail.com>
2019-01-12 22:56:50 +0900

    SnippetDetail CBV구현

--- app/snippets/urls/drf_cbv.py
+++ app/snippets/urls/drf_cbv.py
@@ -4,9 +4,11 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from ..views import drf_cbv as views
 
 # path의 두번째 인수의 함수를 바로 호출
-# 클래스 메서드인 as_view()를 통해 클래스의 메서드를 호
+# 클래스 메서드인 as_view()를 통해 클래스의 메서드를 호출
 urlpatterns = [
     path('snippets/',views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- app/snippets/views/drf_cbv.py
+++ app/snippets/views/drf_cbv.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -18,3 +19,38 @@ class SnippetList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SnippetDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Snippets.objects.get(pk=pk)
+        except Snippets.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = SnippetSerializer(snippet)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None, **kwargs):
+        # kwargs = dict
+        # kwargs[''] = 값을 참조
+        # kwargs.get('partial', False) : defautl를 False
+        # kwargs.pop은 get과 동일하나 인자에서 빼버림
+        partial = kwargs.pop('partial', False)
+        snippet = self.get_object(pk)
+        serializer = SnippetSerializer(snippet, data=request.data, partial=partial)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk, format=None):
+        return self.put(request, pk, format, partial=True)
+
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
